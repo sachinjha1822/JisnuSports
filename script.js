14ac3367417648f49908d4f93fc5eaa6a6e54b03
Sachin Jha <113677887+sachinjha1822@users.noreply.github.com>
2025-02-24 23:54:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+  document.addEventListener("copy", (event) => {
+      const selectData = window.getSelection().toString();
+      event.clipboardData.setData(
+        "text/plain",
+        "SachinJha1822",
+    );
+    event.preventDefault();
+  });
+</script>
+<script>
+  document.addEventListener("contextmenu", function(event){
+    alert("Inspect Elements Not Allowed");
+    event.preventDefault();
+  })
